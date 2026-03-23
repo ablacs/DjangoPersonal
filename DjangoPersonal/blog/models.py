4367bf1a3166e32d657bb85d7c3398cf70a85bb7
ablacs <ablacsxd@gmail.com>
2026-03-23 15:34:25 -0300

    DjangoAdmin

--- DjangoPersonal/blog/models.py
+++ DjangoPersonal/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    STATUS_CHOICES = (
+        (0, 'Rascunho'),
+        (1, 'Publicado'),
+    )
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
+
+    def __str__(self):
+        return self.title
